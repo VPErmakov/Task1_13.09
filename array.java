87bdee864dc2c0a46ae813b6a884605a2bf1ca8e
VPErmakov <90635320+VPErmakov@users.noreply.github.com>
2021-09-13 18:54:05 +0300

    Add files via upload

---
+++ array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class array {
+    public static void main(String[] args) {
+        int[] a={1,5,3,7,10,2,5};
+        System.out.print("[");
+        for (int i=0;i<6;i++)
+            System.out.print(a[i]+", ");
+        System.out.print(a[6]);
+        System.out.print("]");
+    }
+
+
+}
