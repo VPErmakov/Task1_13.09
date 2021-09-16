a0cfd85237708ffa76cffa9ddc7b216a8a3d1c0e
VPErmakov <90635320+VPErmakov@users.noreply.github.com>
2021-09-16 20:57:29 +0300

    Update Task1.java

--- Task1.java
+++ Task1.java
@@ -99,10 +99,12 @@ public class Task1
     // ...
     //правильное решение - 0.5 балла
     public static void printArray(int[][] array){
-    for(int i=0;i<array.length;i++){
-        for(int j=0;j<array.length;j++){
-            System.out.print(array[i][j]);
+     for(int i=0;i<array.length;i++){
+      System.out.print("[");
+      for(int j=0;j<array[i].length-1;j++){
+          System.out.print(array[i][j]+", ");
         }
+      System.out.print(array[i][array[i].length-1]+"]");
     }
 
 
